c6829e8c74884646c36936185943a556ae9c38f6
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-06-06 23:34:06 -0300

    Add dependencies and update server configuratio222n

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 app.use(express.json())
 
-const port = process.env.PORT || 8000;  // Use a porta do ambiente ou 8000 como fallback
+const port = process.env.PORT || 3001;  // Use a porta do ambiente ou 8000 como fallback
 const rotaLivro = require("./Rotas/Livro")
 
 app.use('/livros', rotaLivro)
